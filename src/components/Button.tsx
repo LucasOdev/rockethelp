8adae00ac43cb213d9af761f8b17575f6a764dd6
unknown <lucas.olisboa@outlook.com>
2022-07-19 17:22:28 -0300

    criação da tela de login, e Signin utilizando native base.

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { Button as ButtonNativeBase, IButtonProps, Heading } from 'native-base';
+
+type Props =  IButtonProps & {
+    title: string;
+}
+export function Button({ title, ...rest }: Props) {
+  return (
+    <ButtonNativeBase {...rest}
+    bg="green.700" h={14} fontSize="sm" rounded="sm"
+    _pressed={{bg: "green.500"}}>
+        <Heading color="white" fontSize="sm">{title}</Heading>
+    </ButtonNativeBase>
+  );
+}
