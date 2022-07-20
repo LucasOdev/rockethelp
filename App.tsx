60639dad9e265d0351036897bdfc190aa6ab8ba0
unknown <lucas.olisboa@outlook.com>
2022-07-20 16:24:47 -0300

    finalizadas as interfaces home e register. iniciada details. implementação da navegação

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import { SignIn } from '/Users/Lucas/Downloads/ignite-lab/rockethelp/src/screens/SignIn'
+
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts ({Roboto_400Regular, Roboto_700Bold})
@@ -12,7 +13,7 @@ export default function App() {
       barStyle='light-content'
       backgroundColor="transparent"
       translucent/>
-        { fontsLoaded ? <SignIn /> : <Loading/>}
+        { fontsLoaded ? <Routes/> : <Loading/>}
     </NativeBaseProvider>
     
   );
